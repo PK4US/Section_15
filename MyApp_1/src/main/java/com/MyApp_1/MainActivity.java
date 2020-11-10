567c6e788bd8a2f18ae0c6a3ef1704de6a15c220
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-11-10 16:56:09 +0200

    Урок 1. Получение данных по URL при нажатии на одну из кнопку переход на сайт

--- MyApp_1/src/main/java/com/MyApp_1/MainActivity.java
+++ MyApp_1/src/main/java/com/MyApp_1/MainActivity.java
@@ -1,14 +1,51 @@
 package com.MyApp_1;
 
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button blink1 = findViewById(R.id.blink1);
+        Button blink2 = findViewById(R.id.blink2);
+        Button blink3 = findViewById(R.id.blink3);
+
+        blink1.setOnClickListener(this);
+        blink2.setOnClickListener(this);
+        blink3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.blink1: {
+                WebView wv = findViewById(R.id.wv);
+                wv.getSettings().setJavaScriptEnabled(true);
+                wv.loadUrl("https://www.instagram.com/");
+                break;
+            }
+            case R.id.blink2: {
+                WebView wv = findViewById(R.id.wv);
+                wv.getSettings().setJavaScriptEnabled(true);
+                wv.loadUrl("https://music.youtube.com/");
+                break;
+            }
+            case R.id.blink3: {
+                WebView wv = findViewById(R.id.wv);
+                wv.getSettings().setJavaScriptEnabled(true);
+                wv.loadUrl("http://srs.MyRusakov.ru");
+                break;
+            }
+        }
     }
 }

e0aae47e43b5189af53b550aaa493db7be959017
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-11-10 16:55:11 +0200

    Урок 1. Получение данных по URL

--- Lesson_1/src/main/java/com/Lesson_1/MainActivity.java
+++ Lesson_1/src/main/java/com/Lesson_1/MainActivity.java
@@ -3,6 +3,10 @@ package com.Lesson_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final WebView wv = findViewById(R.id.wv);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadData("<html><head></head><body><h1>Заголовок</h1></body></html>", "text/html", "UTF-8");
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String link = ((EditText) findViewById(R.id.link)).getText().toString();
+                wv.loadUrl(link);
+            }
+        });
     }
 }
